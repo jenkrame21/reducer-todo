b45945547bd63c1360f9cf0dff3d27c20df0f186
Jennifer Kramer <jenkrame@gmail.com>
2021-01-19 15:20:25 -0500

    Created reducer function and initial state in reducer.js file.

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,28 @@
+
+export const initialState = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    }
+];
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case("ADD_TODO"):
+            return [
+                ...state,
+                {
+                    task: action.payload,
+                    completed: false,
+                    id: Date.now()
+                }
+            ];
+        case("TOGGLE_TODO"):
+            return state.map(task => task.id === action.payload ? {...task, completed: !task.completed} : task)
+        case("TODO_CLEAR_COMPLETED"):
+            return state.filter(task => !task.completed)
+        default:
+            return state
+    }
+};
